6ce8e2bb23ec3dbb543c2ad048d5693666ee6e5f
vladislavmedvedev <EnoT3581@mail.ru>
2026-03-27 21:21:31 +0300

    Initial commit

---
+++ src/test/java/me/vladislav/payment_provider/PaymentProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vladislav.payment_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
